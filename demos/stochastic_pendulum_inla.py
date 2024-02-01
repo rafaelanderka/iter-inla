275fba5bb921386e080db832eb5b396527224ce4
So Takao <sotakao@Sos-MacBook-Pro-2.local>
2024-02-01 14:02:46 -0800

    small change

--- demos/stochastic_pendulum_inla.py
+++ demos/stochastic_pendulum_inla.py
@@ -187,8 +187,8 @@ if just_plot is False:
     iter_count = len(model.mse_hist)
 
     # Save result
-    # with open('results/stoch_pendulum_iterated_inla.pickle', 'wb') as f:
-    #     pickle.dump(model, f)
+    with open('results/stoch_pendulum_iterated_inla.pickle', 'wb') as f:
+        pickle.dump(model, f)
 else:
     with open('results/stoch_pendulum_iterated_inla.pickle', "rb") as f:
         model = pickle.load(f)
@@ -218,18 +218,27 @@ plt.show()
 # %%
 from spdeinf.util import cred_wt
 
-# fname = 'demos/pf_baselines/state_posterior_samples_3.npy'
-# posterior_samples = np.load(fname) # Get state samples from the particle smoother
+fname = 'demos/pf_baselines/state_posterior_samples_3.npy'
+posterior_samples = np.load(fname) # Get state samples from the particle smoother
+posterior_samples = posterior_samples[:,:,0]
+N = posterior_samples.shape[1] # Number of samples
+
+# shuffle sample indices for each time step for consistent MMD evaluation
+for t, samples_t in enumerate(posterior_samples):
+    shuffled_idxs = np.random.choice(N, size=(N,), replace=False)
+    posterior_samples[t] = posterior_samples[t][shuffled_idxs]
 
 num_samples = 5000
 final_dist = model.marginal_dist_u_y
 samples = final_dist.sample(num_samples)
 samples = np.reshape(samples, (num_samples, -1))
 
-# print("Computing MMD...")
-# x = posterior_samples[:,:,0].T
-# y = samples
-# mmd = util.MMD(x, y)
+print("Computing MMD... (this takes a minute or two to compute)")
+x = posterior_samples.T
+y = samples
+mmd = util.MMD(x, y)
+
+print(f"MMD = {mmd}")
 
 weights = np.ones(num_samples) / num_samples
 creds = [50, 60, 70, 80, 90, 95]
@@ -250,17 +259,17 @@ plt.plot(T, u[0], c='C1', linewidth=4)
 # plt.plot(T, model.u0[0], c='k', linestyle='--', linewidth=2)
 plt.axvline(obs_loc_1 * dt, color='grey', ls=':', linewidth=2) 
 plt.scatter(idxs * dt, obs_vals, c='k', zorder=10)
-# font = font_manager.FontProperties(family='Times New Roman',
-#                                    style='normal', size=16,
-#                                    weight='bold')
-# plt.text(13, 2, f"MMD = {mmd:.3f}", fontproperties=font)
+font = font_manager.FontProperties(family='Times New Roman',
+                                   style='normal', size=16,
+                                   weight='bold')
+plt.text(13, 2, f"MMD = {mmd:.3f}", fontproperties=font)
 plt.xlabel("$t$", fontsize=12)
 plt.ylabel("$u$", fontsize=12)
 plt.xticks(fontsize=12)
 plt.yticks(fontsize=12)
 plt.tight_layout()
 package_dir = '/Users/sotakao/Dropbox/Mac/Documents/spde-inference'
-# plt.savefig(os.path.join(package_dir, "figures/pendulum/stoch_pendulum_spde_inla_fit_2.pdf"))
+plt.savefig(os.path.join(package_dir, "figures/pendulum/stoch_pendulum_spde_inla_fit_2.pdf"))
 
 
 
